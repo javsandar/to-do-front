001af543accd3a30551612114c760a0e5916be7b
Darío Javega Sansano <dario.javega@bahiasoftware.es>
2023-05-04 11:58:09 +0200

    Revert "segundo commit"
    This reverts commit ca9f720a7cb213d88c1391c7339dcec4a286d756.

--- src/presentation/TodoPage.js
+++ src/presentation/TodoPage.js
@@ -8,12 +8,29 @@ export function TodoPage() {
     const [todos, setTodos] = useState([]);
    /* renderTodos es llamado en dos casos, al inicio de la aplicación como 
       carga inicial de las tareas y cada vez que se añada una tarea */
-
+    const renderTodos = () => {
+        axios.get('/todos?finished=false').then((response) => {
+            if(response.data.length !== 0)
+                setTodos(response.data);
+            else {
+                return;
+            }
+        });
+    }
     
     useEffect(() => {
         renderTodos();
     },[])
     
+    const createTodo = () => {
+        const body = {
+            text: todoText,
+            finished: false,
+        };
+        const headers = {'Content-Type': 'application/json'};
+        axios.post('/todos', body, headers).then(() => renderTodos());
+        setTodoText('');
+    }
    
     const handleChange = (event) => {
         setTodoText(event.target.value);
